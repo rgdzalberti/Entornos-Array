8194cbda2a5c59a8e46525d7626c841c693894c6
Ricardo <rgallego@iesrafaelalberti.es>
2022-01-19 18:30:27 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,14 +2,13 @@ import java.util.*
 
 fun main() {
 
-    //Creo los vectores y los meto en una lista para despues poder meterlos con un bucle y no a mano.
+    //Creo los vectores
     val vector1 =  Vector<Int>();
     val vector2 =  Vector<Int>();
-    val listaVectores = mutableListOf<Vector<Int>>()
-    listaVectores.add(vector1); listaVectores.add(vector2)
 
     //Row - Column
     val matriz = Array(4) { IntArray(1) }
+
 
     println("A continuación introduce los valores de los vectores")
     for (i in 0..4) {print("Valor [$i]: "); var input = readLine() ;vector1.add(input?.toInt())}
@@ -17,12 +16,19 @@ fun main() {
     for (i in 0 until 4) {print("Valor [$i]: "); var input = readLine() ;vector2.add(input?.toInt())}
 
     //Meto los valores de los vectores en la matriz
-    for (j in 0 until 2) {
-        for (i in 0 until vector1.size) { matriz[j][i] = (listaVectores[j].elementAt(i))}
-    }
+
+    for (i in 0..4) {matriz[0][i] = vector1[i]}
+    for (i in 0..3) {matriz[1][i] = vector1[i]}
+
 
     //Muestro la matriz
+    /*
     for (j in 0 until 2){
         for (i in 0 until matriz.size) {println(matriz[j][i])}
+    }
+
+     */
+    for (i in matriz.indices) {
+        println(matriz[i].contentToString())
     }
 }
