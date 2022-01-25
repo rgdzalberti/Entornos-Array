64353a4fabf592280be5e9f61324af4a295c6e82
Ricardo <rgallego@iesrafaelalberti.es>
2022-01-25 17:29:39 +0100

    .

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,29 +2,37 @@ import java.util.*
 
 fun main() {
 
-    //Creo los vectores
     val vector1 =  Vector<Int>();
     val vector2 =  Vector<Int>();
 
-    //Column - Row
-    val matriz = Array(2) {Array(5) {0} }
+    //Son las constantes que usaré en la fórmula siguiente.
+    val c1: Int = 5;
+    val c2: Int = 6;
 
-
-    println("A continuación introduce los valores de los vectores")
-    for (i in 0..4) {print("Valor [$i]: "); var input = readLine() ;vector1.add(input?.toInt())}
+    println("A continuación introduce la longitud de los vectores")
+    println("-----Primer vector-----")
+    print("Longitud: ");var inputv1 = readLine()?.toInt()!!
+    for (i in 0 until inputv1) {var valor = i * 2 + c1 ;vector1.add(valor.toInt())}
     println("-----Segundo vector-----")
-    for (i in 0 until 4) {print("Valor [$i]: "); var input = readLine() ;vector2.add(input?.toInt())}
+    print("Longitud: ");var inputv2 = readLine()?.toInt()!!
+    for (i in 0 until inputv2) {var valor = i * 2 + c2 ;vector2.add(valor.toInt())}
+
+    //Utilizaré longitud para comprobar cual de los dos vectores es más largo y así en los siguientes bucles saber cuando parar
+    var longitud = inputv1
+    if (inputv1<inputv2) {longitud = inputv2}
+
+    //Column - Row
+    val matriz = Array(2) {Array(longitud) {0} }
 
     //Meto los valores de los vectores en la matriz
 
-    for (i in 0..4) {matriz[0][i] = vector1[i]}
-    for (i in 0..3) {matriz[1][i] = vector2[i]}
-
+    for (i in 0 until inputv1) {matriz[0][i] = vector1[i]}
+    for (i in 0 until inputv2) {matriz[1][i] = vector2[i]}
 
     //Muestro la matriz
     println("")
     for (j in 0 until 2) {
-        for (i in 0 until 5) {print("["); print(matriz[j][i]); print("] "); if (i == 4) {println("")}}
+        for (i in 0 until longitud) {print("["); print(matriz[j][i]); print("] "); if (i == longitud - 1) {println("")}}
     }
 
 }
